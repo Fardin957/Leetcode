80335423e4224f56a83c61c23d2d7b6feb15dbd2
Fardin957 <fardinfaruk95@gmail.com>
2025-11-09 14:51:53 +0600

    Time: 27 ms (87.57%), Space: 58.4 MB (9.56%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,8 +2,8 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
          Arrays.sort(nums);
          List<List<Integer>> result =new ArrayList<>() ;
-         for(int i=0; i<nums.length && nums[i]<0;i++){
-            if(i==0|| nums[i] != nums[i-i]){
+         for(int i=0; i<nums.length && nums[i]<=0;i++){
+            if(i==0 || nums[i] != nums[i-1]){
                 twoSum2(nums,i,result);
 
             }
@@ -23,7 +23,7 @@ class Solution {
                 right--;
 
             }
-              else{
+             else{
                    result.add(Arrays.asList(nums[i] ,nums[left++],nums[right--]));
                    while(left<right && nums[left] == nums[left-1]){
                     ++left;
