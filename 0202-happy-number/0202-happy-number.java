2982179d3ac9554a065bf8c1f2dde4e795b64ec4
Fardin957 <fardinfaruk95@gmail.com>
2025-11-16 17:07:05 +0600

    Time: 1 ms (68.92%), Space: 42.7 MB (7.19%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,23 +1,51 @@
+//This is hashset code:
 class Solution {
     public boolean isHappy(int n) {
-        int slow = n;
-        int fast = n;
-
-        do {
-            slow = sumOfSquares(slow);
-            fast = sumOfSquares(sumOfSquares(fast));
-        } while (slow != fast);
-
-        return slow == 1;
+        HashSet<Integer> seen = new HashSet<>();
+        while (n != 1) {
+            if (seen.contains(n)) {         // Here we can use seen.contains for detect the loop
+                return false;               // detect the loop
+            }
+            seen.add(n);
+            n = sumOfSquares(n);
+        }
+        return true;
     }
-
-    private int sumOfSquares(int number) {
+    private int sumOfSquares(int num) {
         int sum = 0;
-        while (number > 0) {
-            int digit = number % 10;
+        while (num > 0) {
+            int digit = num % 10;
             sum += digit * digit;
-            number /= 10;
+            num /= 10;
         }
         return sum;
     }
 }
+
+
+//---------------------------------------------------------------------------------------------------------------------------
+// The two pointer code is given below:
+
+// class Solution {
+//     public boolean isHappy(int n) {
+//         int slow = n;
+//         int fast = n;
+
+//         do {
+//             slow = sumOfSquares(slow);
+//             fast = sumOfSquares(sumOfSquares(fast));
+//         } while (slow != fast);
+
+//         return slow == 1;
+//     }
+
+//     private int sumOfSquares(int number) {
+//         int sum = 0;
+//         while (number > 0) {
+//             int digit = number % 10;
+//             sum += digit * digit;
+//             number /= 10;
+//         }
+//         return sum;
+//     }
+// }
