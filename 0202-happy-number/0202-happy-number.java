d7581043310fd266e2db2cc705bf110b679496f6
Fardin957 <fardinfaruk95@gmail.com>
2025-11-16 17:01:37 +0600

    Time: 1 ms (68.92%), Space: 42.1 MB (18.7%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean isHappy(int n) {
-        HashSet<Integer> seen = new HashSet<>();
-        while (n != 1) {
-            if (seen.contains(n)) {         // Here we can use seen.contains for detect the loop
-                return false;               // detect the loop
-            }
-            seen.add(n);
-            n = sumOfSquares(n);
-        }
-        return true;
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = sumOfSquares(slow);
+            fast = sumOfSquares(sumOfSquares(fast));
+        } while (slow != fast);
+
+        return slow == 1;
     }
+
     private int sumOfSquares(int num) {
         int sum = 0;
         while (num > 0) {
